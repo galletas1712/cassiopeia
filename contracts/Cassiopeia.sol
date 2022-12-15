8609c294ace56064372f0a383c656112946d8275
Dionysis Zindros <dionyziz@gmail.com>
2022-12-14 18:34:15 -0800

    Mid-way through incentives

--- contracts/Cassiopeia.sol
+++ contracts/Cassiopeia.sol
@@ -10,17 +10,34 @@ struct Secret {
     uint256 unlockTime;
     G1Point[] a_i;
     PVSSLib.PVSSDecryptedShare[] decryptedShares;
+    uint256 value;
+    mapping(address => bool) shareSubmitted;
+    uint256 countSharesSubmitted;
+    mapping(address => bool) collateralDeposited;
+    uint256 insuranceValue;
+    bool insurancePaid;
+    address dealer;
 }
 
 contract Cassiopeia {
+    uint256 constant GRACE_PERIOD = 100; // in number of blocks
+    // maximum risk free rate for any period of time across all committee members
+    uint256 constant RISK_FREE_RATE = 23; // per block, 0.0023%
+
     SNARKVerifier verifier;
 
     uint256 public n;
     uint256 public t;
-    G2Point[] internal pks;
+    G2Point[] internal pks; // TODO: map committee member Ethereum addresses to pks
     Secret[] internal secrets;
+    mapping(address => uint256) committeeBalances;
+    uint256 liabilities;
 
     event SharedSecret(uint256 secretID);
+
+    function topUp() payable {
+        committeeBalances[msg.sender] += msg.value;
+    }
 
     constructor(uint256 _t, G2Point[] memory _pks, SNARKVerifier _verifier) {
         n = _pks.length;
@@ -32,12 +49,24 @@ contract Cassiopeia {
         verifier = _verifier;
     }
 
+    // TODO: make this support witness encryption
     function shareSecret(
         uint256 unlockTime,
         PVSSLib.PVSSCiphertext memory c,
         uint256 H,
-        SNARKVerifyLib.Proof memory proof
-    ) public returns (uint256) {
+        SNARKVerifyLib.Proof memory proof,
+        uint256 insuranceValue
+    ) public payable returns (uint256) {
+        // rationality requirement for committee members
+        require(
+            // TODO: clean up the math here
+            (n - t + 1) * msg.value > n * insuranceValue * ((1 + r/10000)**unlockTime - 1)
+            // easier version:
+            // (n - t + 1) * msg.value > n * insuranceValue * ((1 + r)*unlockTime - 1)
+        );
+        require(
+            this.balance >= 
+        );
         require(
             SNARKVerifyLib.verifyProof(
                 verifier,
@@ -51,6 +80,7 @@ contract Cassiopeia {
 
         // Add secret to storage
         Secret storage secret = secrets.push();
+        secret.dealer = msg.sender;
         secret.unlockTime = unlockTime;
         for (uint256 i = 0; i < c.a_i.length; i++) {
             secret.a_i.push(c.a_i[i]);
@@ -58,20 +88,62 @@ contract Cassiopeia {
         emit SharedSecret(secrets.length - 1);
         return secrets.length - 1;
     }
+    
+    // TODO: turn 3 period checks (before unlock, during grace period, after grace period) into modifiers
 
     function submitShare(
         uint256 secretID,
         uint256 index,
         G2Point memory decrypted
-    ) public {
+    ) public inGracePeriod(secretID) {
         require(secretID < secrets.length, "Secret does not exist");
         require(index < n, "Index out of bounds");
-        require(block.timestamp >= secrets[secretID].unlockTime);
+        require(!secret[secretID].sharedSubmitted[index]);
+
         PVSSLib.verifyShare(secrets[secretID].a_i[index], decrypted);
 
         secrets[secretID].decryptedShares.push(
             PVSSLib.PVSSDecryptedShare(index, decrypted)
         );
+
+        secret[secretID].sharesSubmitted[index] = true;
+        ++secret[secretID].countSharesSubmitted;
+    }
+
+    modifier beforeUnlock(uint256 secretID) {
+        require(block.number < secret[secretID].unlockTime);
+        _;
+    }
+
+    modifier inGracePeriod(uint256 secretID) {
+        require(block.number >= secret[secretID].unlockTime);
+        require(block.number < secret[secretID].unlockTime + GRACE_PERIOD);
+        _;
+    }
+
+    modifier afterGracePeriod(uint256 secretID) {
+        require(block.number >= secret[secretID].unlockTime + GRACE_PERIOD);
+        _;
+    }
+
+    function reclaimMemberCollateral(uint256 secretID) afterGracePeriod(secretID) {
+    }
+
+    function reclaimDealerCollateral(uint256 secretID) afterGracePeriod(secretID) {
+        require(!secret[secretID].insurancePaid);
+        secret[secretID].insurancePaid = true;
+
+        uint256 amount;
+
+        if (secret[secretID].countSharesSubmitted >= t) {
+            // happy path
+
+        }
+        else {
+
+        }
+
+        secret[secretID].dealer.send(amount);
     }
 
     function getPK(uint256 i) public view returns (G2Point memory) {
