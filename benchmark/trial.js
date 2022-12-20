11104df1708a80fbdd46a6a2789c8a2e6d61bd15
Schwinn Saeree <17022745+galletas1712@users.noreply.github.com>
2022-12-20 05:27:56 +0000

    chore: cleanup benchmark

--- benchmark/trial.js
+++
@@ -1,8 +0,0 @@
-// import { deploy, shareValidSecret } from "../cassiopeia_lib";
-
-module.exports = async ({n, t}) => {
-  const { all_keys, cassiopeia } = await deploy(n, t);
-  const gas1 = (await shareValidSecret(n, t, all_keys, cassiopeia)).receipt.gasUsed.toString();
-  const gas2 = (await shareValidSecret(n, t, all_keys, cassiopeia)).receipt.gasUsed.toString();
-  return { gas1, gas2 };
-};
