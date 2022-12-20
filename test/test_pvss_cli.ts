c7017354fcc891ff0287b691059e6eb31502aab5
Schwinn Saeree <17022745+galletas1712@users.noreply.github.com>
2022-12-20 02:14:45 -0800

    feature: test PVSS for different result upon corruption of share

--- test/test_pvss_cli.ts
+++ test/test_pvss_cli.ts
@@ -1,17 +1,17 @@
 import { execFileSync } from "child_process";
 import { expect } from "chai";
-import { shuffled } from "ethers/lib/utils";
+import { shuffled, randomBytes, hexlify } from "ethers/lib/utils";
 
 const BINARY = "pvss/target/debug/cassiopeia";
 
 describe("PVSS CLI", () => {
-  const testRunEndToEnd = (n: number, t: number) => {
-    const all_keys = JSON.parse(
+  const setup = (n: number, t: number) => {
+    const allKeys = JSON.parse(
       execFileSync(BINARY, ["gen-keys", n.toString()]).toString()
     );
-    const pvss_output = JSON.parse(
+    const pvssOutput = JSON.parse(
       execFileSync(BINARY, ["deal-secret", t.toString()], {
-        input: JSON.stringify(all_keys.pks),
+        input: JSON.stringify(allKeys.pks),
       }).toString()
     );
     let indices = shuffled([...Array(n).keys()]);
@@ -21,29 +21,54 @@ describe("PVSS CLI", () => {
         execFileSync(BINARY, ["decrypt-share"], {
           input: JSON.stringify({
             i: i,
-            ciphertext: pvss_output.ciphertext,
-            sk: all_keys.sks[i],
+            ciphertext: pvssOutput.ciphertext,
+            sk: allKeys.sks[i],
           }),
         }).toString()
       ),
     ]);
-    for (let numRevealed = 0; numRevealed <= n; numRevealed++) {
-      const decryptedSecret = JSON.parse(
-        execFileSync(BINARY, ["combine-shares"], {
-          input: JSON.stringify(decryptedShares.slice(0, numRevealed)),
-        }).toString()
-      );
-      if (numRevealed < t) {
-        expect(decryptedSecret).to.not.deep.equal(pvss_output.secrets.h_f_0);
-      } else {
-        expect(decryptedSecret).to.deep.equal(pvss_output.secrets.h_f_0);
-      }
-    }
-  };
+		return { allKeys, pvssOutput, decryptedShares };
+  }
+
+	const decryptSecret = (decryptedShares: any[]) => JSON.parse(
+		execFileSync(BINARY, ["combine-shares"], {
+			input: JSON.stringify(decryptedShares),
+		}).toString()
+	);
 
   it("Should recover a valid secret if and only if number of revealed shares >= t", () => {
     const n = Math.floor(Math.random() * 25) + 1;
     const t = Math.floor(Math.random() * n) + 1; // Between 1 and n inclusive
-    testRunEndToEnd(n, t);
+		const { allKeys, pvssOutput, decryptedShares } = setup(n, t);
+    for (let numRevealed = 0; numRevealed <= n; numRevealed++) {
+			const decryptedSecret = decryptSecret(decryptedShares.slice(0, numRevealed));
+      if (numRevealed < t) {
+        expect(decryptedSecret).to.not.deep.equal(pvssOutput.secrets.h_f_0);
+      } else {
+        expect(decryptedSecret).to.deep.equal(pvssOutput.secrets.h_f_0);
+      }
+    }
   });
+
+	it("Should print out different result if at least one invalid share incorporated", () => {
+    const n = Math.floor(Math.random() * 23) + 3;
+		const { allKeys, pvssOutput, decryptedShares } = setup(n, n - 1);
+
+		expect(decryptSecret(decryptedShares)).to.deep.equal(pvssOutput.secrets.h_f_0);
+		expect(decryptSecret(decryptedShares.slice(0, n - 1))).to.deep.equal(pvssOutput.secrets.h_f_0);
+		expect(decryptSecret(decryptedShares.slice(0, n - 2))).to.not.deep.equal(pvssOutput.secrets.h_f_0);
+
+		const randBytes = randomBytes(32);
+		const sub = hexlify([0, ...randBytes.slice(1)]);
+		const newDecryptedShares = [...decryptedShares.slice(0, n - 1),
+			[
+				decryptedShares[n - 1][0],
+				{
+					x: [sub, sub],
+					y: [sub, sub],
+				}
+			]
+		];
+		expect(decryptSecret(newDecryptedShares)).to.not.deep.equal(pvssOutput.secrets.h_f_0);
+	});
 });
